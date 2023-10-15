1127000ee1115a678746233efcbb715ca4585c2e
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-10-15 22:56:54 +0100

    new

--- src/form.js
+++ src/form.js
@@ -2,21 +2,14 @@ import Input from "./input";
 import Button from "./button";
 
 function From() {
-  const red = {
-    color: "red",
-  };
-  const blue = {
-    color: "green",
-  };
-  const c = 20 ? blue.color : red.color;
   return (
     <>
       <div>
         <Input />
         <br />
         <Button />
-        <p style={c}>Hello</p>
-        <p> </p>
+        <p>Hello</p>
+        <p>world </p>
       </div>
     </>
   );
