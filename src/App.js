61cef1407e24c77229813edd8ab7a40e439a37ac
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-10-15 14:00:20 +0100

    new file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Header from "../../learning-react/src/header.js";
 function App() {
   return (
     <div className="App">
       {" "}
       <p>Hello world!! </p>
-      <Header />
     </div>
   );
 }
