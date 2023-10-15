341f84a45a91b21ac8a6157d3e0489ab72c946a1
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-10-15 22:03:00 +0100

    new file

--- src/form.js
+++ src/form.js
@@ -6,15 +6,19 @@ function Next() {
 function Back() {
   return <h1>Hello world</h1>;
 }
+
 function From() {
-  let classes = "ezekiel" == "ezekiel" ? <Next /> : <Back />;
   return (
     <>
       <div>
         <Input />
         <br />
         <Button />
-        <p>{classes}</p>
+        <p>Hello</p>
+        <p>
+          {" "}
+          "ezekiel" === "ezekiel" ? <Next /> : <Back />;
+        </p>
       </div>
     </>
   );
