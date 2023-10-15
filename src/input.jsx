4bcc5ccbfb63b46e4d58b49dabc84d78be275cb2
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-10-15 17:52:32 +0100

    new file

---
+++ src/input.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./button";
+function Name() {
+  return (
+    <>
+      <div>
+        <input type="text" name="" id="" />
+        <br />
+        <Button />
+      </div>
+    </>
+  );
+}
+export default Name;
