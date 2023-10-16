ada2bfb907fc89e016f2cf8f5648c94e932f4e03
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-10-16 23:25:05 +0100

    n

--- src/form.js
+++ src/form.js
@@ -3,6 +3,7 @@ import Button from "./button";
 const buttonMap = [
   { id: 1, value: 0 },
   { id: 1, value: 0 },
+  { id: 1, value: 0 },
 ];
 let mapU = buttonMap.map((e) => <Button key={e.id} />);
 function From() {
